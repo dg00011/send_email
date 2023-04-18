62a35042f56ae00339629b78c4b03a4536da8f39
Dheeraj Gupta <dheerajg@digitizeco.in>
2023-04-18 16:49:31 +0530

    ...

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
 const express = require('express')
-const app =express()
+const app = express()
+const https = require('https');
+
+app.get("/", (req, res) => {
 
 
-app.get("/",(req,res)=>{
-    res.send({message:'Hey'})
+    console.loga('req',req,req.body)
+    // res.send({message:'Hey'})
+
+    //if (Object.keys(req.query).length > 0) {
+
+    //const configurationLink = `${req.query.configuration_link}${req.query.threekit_short_id}`;
+
+
+    const data = JSON.stringify({
+        From: 'DNA@jlgloveco.com',
+        To: 'dheerajg@digitizeco.in',
+        MessageStream: 'jl-gloves',
+        TemplateAlias: 'code-your-own-1',
+        TemplateModel: {
+            customer_name: 'we are in index',
+            customer_email: "req.query.email",
+            customer_phone: "req.query.phone",
+            configuration_id: "req.query.configuration_id",
+            configuration_link: "configurationLink",
+            snapshot: req.query.snapshot,
+        },
+    });
+
+
+
+    const options = {
+        hostname: 'api.postmarkapp.com',
+        path: '/email/withTemplate',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Postmark-Server-Token': '8100fc97-2fd9-4472-85f5-7df8d87f71b2',
+        },
+    };
+
+
+    const request = https.request(options, (response) => {
+
+        let responseBody = '';
+        response.on('data', (chunk) => {
+            responseBody += chunk;
+        });
+
+
+        response.on('end', () => {
+            console.log(responseBody);
+        });
+
+    });
+
+
+    request.on('error', (error) => {
+        console.error(error);
+    });
+
+
+    request.write(data);
+    request.end();
+
+    // }
+
 })
 
 
